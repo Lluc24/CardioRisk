78a202ae67d366f8fd000dbf336e9f29cde7f44e
Aleksandra Zawadzka <aleksandra.zawadzka@epfl.ch>
2025-10-06 11:35:16 +0200

    evaluation pipeline

--- evalution.py
+++ evalution.py
@@ -1,21 +1,48 @@
 # Evaluation Pipeline
 # input - array of 0 and 1s evaluated based on the split out training set
-# output - mean square error loss function, accuracy, recall, true positive rate, true negative rate
+# output - mean square error loss function, accuracy, true positive rate, true negative rate, f1 score
 
 
 def accuracy(y_true,y_predicted):
-    accuracy = metrics.accuracy_score(y_true, y_predicted)
+    ' accuracy = #correct_predictions/total_predictions '
+    corrects=np.where(y_true == y_predicted,1,0)
+    corrects_num=np.sum(corrects)
+    total_num=np.sum(np.ones(y_true.shape))
+    output=corrects_num/total_num    
+    return output
 
-def accuracy_score(y_true, y_pred, *, normalize=True, sample_weight=None):
-    """Accuracy gives number of correct predictions divided by total number of predictions.
+def precision(y_true,y_predicted):
+    'precision = (FP)/(TP+FP) '
+    TP=np.sum(np.where((y_predicted==1) & (y_true==1),1,0))
+    FP=np.sum(np.where((y_predicted==1) & (y_true==-1),1,0))
+    output=TP/(TP+FP)
+    return output
 
-    y_true : 1d array-like
-    y_pred : 1d array-like
+def tpr(y_true,y_predicted):
+    ' true positive rate = (TP)/(TP+FN) (aka recall)'
+    TP=np.sum(np.where((y_predicted==1) & (y_true==1),1,0))
+    FN=np.sum(np.where((y_predicted==-1) & (y_true==1),1,0))
+    output=TP/(TP+FN)
+    return output 
 
-    """
+def fpr(y_true,y_predicted):
+    ' false positive rate = (FP)/(FP+TN) '
+    FP=np.sum(np.where((y_predicted==1) & (y_true==-1),1,0))
+    TN=np.sum(np.where((y_predicted==-1) & (y_true==-1),1,0))
+    output=FP/(FP+TN)
+    return output
 
+def f1(y_true,y_predicted):
+    ' f1 = 2* (precision * recall) / (precision + recall) '
+    prec=precision(y_true,y_predicted)
+    rec=tpr(y_true,y_predicted)
+    output=2*(prec*rec)/(prec+rec)
+    return output 
 
-
-#    fpr, tpr, threshold = metrics.roc_curve(y_true, y_predicted)
-#    f1=metrics.f1_score(y_true, y_predicted)
-#    return accuracy, fpr, tpr, f1
+def metrics_summary(y_true,y_predicted):
+    acc=accuracy(y_true,y_predicted)
+    prec=precision(y_true,y_predicted)
+    tprate=tpr(y_true,y_predicted)
+    fprate=fpr(y_true,y_predicted)
+    f1score=f1(y_true,y_predicted)
+    return acc,prec,tprate,fprate,f1score
