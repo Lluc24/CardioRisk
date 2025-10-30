f78432aefc322447e05e9b1dee5acd77da1c0734
siscubl04 <siscubadial@gmail.com>
2025-10-30 21:02:33 +0100

    Corrections in  the standarization of local and aicrowd test sets

--- run.py
+++ run.py
@@ -23,27 +23,28 @@ def main():
     dataset = Dataset(x_train, y_train, num_cont_features)
 
     model = LogisticRegressionGD()
-    metrics = cross_validate(model, dataset, threshold=0.18)
+    metrics = cross_validate(model, dataset, search_threshold_iterations=1000)
+    #metrics = cross_validate(model, dataset, k_fold=5, threshold=0.19)
 
     # Average weights and threshold from cross-validation
     weights = np.mean(metrics.pop('Weights'), axis=0)
     threshold = float(np.mean(metrics.pop('Thresholds')))
 
     # Average mean and std from cross-validation
-    mean = np.mean(metrics.pop('Mean'))
-    std = np.mean(metrics.pop('Std'))
+    mean = np.mean(metrics.pop('Mean'), axis=0)
+    std = np.mean(metrics.pop('Std'), axis=0)
+
+    model.w = weights  # Set model weights
 
     # Predict on local test set
     x_test_local, y_test_local = dataset.get_test_set()
-    x_test_local[:, 1:num_cont_features] = (x_test_local[:, 1:num_cont_features] - mean) / std  # Standardize test set
+    x_test_local[:, 1:num_cont_features+1] = (x_test_local[:, 1:num_cont_features+1] - mean) / std  # Standardize test set
     y_te_local_pred = model.predict(x_test_local, threshold=threshold)
     local_metrics = model.get_metrics(y_test_local, y_te_local_pred)
     print("Local Test Set Metrics:", local_metrics)
 
     # Predict on online test set
-
-    model.w = weights  # Set model weights
-    x_test[:, 1:num_cont_features] = (x_test[:, 1:num_cont_features] - mean) / std  # Standardize test set
+    x_test[:, 1:num_cont_features+1] = (x_test[:, 1:num_cont_features+1] - mean) / std  # Standardize test set
     y_te_pred = model.predict(x_test, threshold=threshold)
 
     # Create submission file
