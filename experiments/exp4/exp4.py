58ae871a95be7d6bd50cdb383374172d8804ea52
Lluc Santamaria Riba <lluc.santa@gmail.com>
2025-10-31 04:18:21 +0100

    Exp4: The lambda_RG

--- experiments/exp4/exp4.py
+++ experiments/exp4/exp4.py
@@ -1,42 +1,35 @@
-from collections import defaultdict
 import csv
 from data_cleaning import Data
-from implementations import compute_gradient, mean_squared_error
 from dataset import Dataset
 import numpy as np
 
 from methods import cross_validate
-from models import RegularizedLogisticRegressionGD
+from models import LogisticRegressionGD
 
-MAX_ITERS = 200
-GAMMA = 0.7
-LAMBDAS = [0.0000000001, 0.000001, 0.001, 0.1]
-DEGREE = 3
+MAX_ITERS = 300
+GAMMAS = np.logspace(-2, 0, 10)
 
-def run_exp4():
+def run_exp1():
     print("Running Experiment 4")
     data = Data()
-    data.load_from_csv("dataset", "metadata.json")
-    data.feature_expansion(DEGREE)
+    #data.load_from_csv("dataset", "metadata.json")
+    data.load_from_numpy_file("cleaned_data.npz")
     data.add_intercept()
     dataset = Dataset(data.x_train, data.y_train, data.num_cont_features)
 
-    with open(f"exp4.csv", "w") as csvfile:
+    with open(f"lambda_RG.csv", "w") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["Lambda", "Training Loss", "Validation Loss", "Weights Norm", "F1 Score", "Accuracy", "Threshold"])
+        writer.writerow(["Lambda", "F1 Score", "Accuracy", "Threshold"])
 
-        for lambda_ in LAMBDAS:
-            model = RegularizedLogisticRegressionGD(max_iters=MAX_ITERS, gamma=GAMMA, lambda_=lambda_)
-            metrics = cross_validate(model, dataset, add_weights=True, search_threshold_iterations=100)
+        for gamma in GAMMAS:
+            model = LogisticRegressionGD(max_iters=MAX_ITERS, gamma=gamma)
+            metrics = cross_validate(model, dataset, add_weights=False, search_threshold_iterations=100)
 
-            tr_loss = np.mean(metrics["Train Loss"]).item()
-            val_loss = np.mean(metrics["Validation Loss"]).item()
-            w_norm = np.mean(metrics["Weights"], axis=(1, 0)).item()
             acc = np.mean(metrics["Accuracy"]).item()
             f1 = np.mean(metrics["F1 Score"]).item()
             threshold = np.mean(metrics["Thresholds"]).item()
 
-            writer.writerow([lambda_, tr_loss, val_loss, w_norm, f1, acc, threshold])
+            writer.writerow([gamma, f1, acc, threshold])
 
 if __name__ == "__main__":
-    run_exp4()
+    run_exp1()
